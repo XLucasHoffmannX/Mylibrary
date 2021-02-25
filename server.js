682f0980f7559c7ea6acc83611cdde8885ffa500
Lucas Hoffmann <henriquelucashoffmann@gmail.com>
2021-02-25 14:31:18 -0300

    Rotas autores iniciada

--- server.js
+++ server.js
@@ -4,18 +4,21 @@ if(process.env.NODE_ENV != 'production'){ require('dotenv').config() }
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 // dependencies interns
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 //app
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname+'/views');
 app.set('layout', 'layouts/layout');
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 app.use(expressLayouts);
 app.use(express.static('public'));
-app.use('/', indexRouter)
+
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
@@ -25,5 +28,8 @@ mongoose.connect(process.env.DATABASE_URL, {
 const db = mongoose.connection
 db.on('error', error=>console.log(error))
 db.once('open', ()=>console.log('Connected to Mogoose'))
+
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000, console.log('Server in on port 3000'))
